307ea0d58203458dcab5125b4170372874775179
rasonpang <pangshengwei99.work@gmail.com>
2022-10-24 23:04:38 +0800

    fix carousel threshold value

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,5 +1,5 @@
-import { createSignal, onMount } from 'solid-js';
-import styles from './style.module.css';
+import { createSignal, onMount } from "solid-js";
+import styles from "./style.module.css";
 
 const Carousel = (props) => {
     const [selectedKey, setSelectedKey] = createSignal(0);
@@ -13,20 +13,23 @@ const Carousel = (props) => {
 
     // Intersection Observer
     const assignObserverToSection = () => {
-        const imageSections = document.querySelectorAll('section');
+        const imageSections = document.querySelectorAll("section");
 
-        const imageObserver = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    const newIndex = entry.target.getAttribute('index');
-                    setSelectedKey(newIndex);
-                }
-            })
-        }, {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.3,
-        });
+        const imageObserver = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        const newIndex = entry.target.getAttribute("index");
+                        setSelectedKey(newIndex);
+                    }
+                });
+            },
+            {
+                root: null,
+                rootMargin: "0px",
+                threshold: 0.5,
+            }
+        );
 
         imageSections.forEach((el) => imageObserver.observe(el));
     };
@@ -34,28 +37,30 @@ const Carousel = (props) => {
     return (
         <div className={styles.container}>
             <div className={styles.images_wrapper}>
-                {
-                    data.map(({ imgSrc, url }, index) => (
-                        <a className={styles.image_section} href={url}>
-                            <section index={index}>
-                                <img className={styles.image} src={imgSrc} />
-                            </section>
-                        </a>
-                    ))
-                }
+                {data.map(({ imgSrc, url }, index) => (
+                    <a className={styles.image_section} href={url}>
+                        <section index={index}>
+                            <img className={styles.image} src={imgSrc} />
+                        </section>
+                    </a>
+                ))}
             </div>
             <div className={styles.indicator_wrapper}>
-                {
-                    [...Array(data.length).keys()].map((i) => (
-                        <span className={`
+                {[...Array(data.length).keys()].map((i) => (
+                    <span
+                        className={`
                             ${styles.indicator}
-                            ${selectedKey() == i ? styles.indicator_selected : ''}
-                        `}></span>
-                    ))
-                }
+                            ${
+                                selectedKey() == i
+                                    ? styles.indicator_selected
+                                    : ""
+                            }
+                        `}
+                    ></span>
+                ))}
             </div>
         </div>
-    )    
+    );
 };
 
 export default Carousel;
